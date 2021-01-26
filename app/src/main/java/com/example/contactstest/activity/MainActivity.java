2d01d8fc99adf0ada18a386149970a291e50f68c
yuanbao97 <316261393@qq.com>
2021-01-26 15:24:12 +0800

    init

---
+++ app/src/main/java/com/example/contactstest/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.contactstest.activity;
+
+import android.os.Bundle;
+
+import com.example.contactstest.R;
+import com.example.contactstest.framework.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
